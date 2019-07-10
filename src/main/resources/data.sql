a160fad956999e1ce0760bee8d902dce114e754b
avajhubgit <dima893@gmail.com>
2019-07-10 11:55:14 +0300

    Add birthday.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,8 +5,8 @@ values (2, 'Testing');
 insert into department(department_id, department_name)
 values (3, 'Financial');
 
-insert into employee(employee_id, first_name, last_name, department_id)
-values (1, 'John', 'Smith', 1);
+insert into employee(employee_id, first_name, last_name, birthday, department_id)
+values (1, 'John', 'Smith', '2001-10-22', 1);
 
-insert into employee(employee_id, first_name, last_name, department_id)
-values (2, 'Joe', 'Thomson', 2);
+insert into employee(employee_id, first_name, last_name, birthday, department_id)
+values (2, 'Joe', 'Thomson', '2000-05-18', 2);
